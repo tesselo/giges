5e98f814f11c0890b997897ede5b31eb8b64f9cb
Daniel Moreno <daniel.moreno@tesselo.com>
2021-07-01 12:19:29 +0200

    chore(clean): Comply with mypy

--- giges/app.py
+++ giges/app.py
@@ -1,15 +1,16 @@
+import connexion
 from connexion.apps.flask_app import FlaskApp
 from connexion.resolver import RestyResolver
 from flask import Flask
 
 
 class App(FlaskApp):
-    def create_app(self):
+    def create_app(self) -> Flask:
         app = Flask(__name__)
         return app
 
 
-def create_connexion_app():
+def create_connexion_app() -> connexion.App:
     connexion_app = App(__package__, specification_dir="schemas/")
 
     connexion_app.add_api(
@@ -22,5 +23,5 @@ def create_connexion_app():
     return connexion_app
 
 
-def create_flask_app():
+def create_flask_app() -> Flask:
     return create_connexion_app().app
