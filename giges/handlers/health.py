713ddbfb6fcb34f5a72044ecc92681a6e67ff6a2
Daniel Moreno <daniel.moreno@tesselo.com>
2021-07-14 11:36:18 +0200

    docs(health): add docstring for the ping handler

--- giges/handlers/health.py
+++ giges/handlers/health.py
@@ -2,4 +2,9 @@ from typing import Dict, Tuple
 
 
 def ping() -> Tuple[Dict[str, bool], int]:
+    """
+    A trivial response to verify that the service is working.
+
+    :return: a tuple with the response body and the status
+    """
     return {"pong": True}, 200
