b4f8c4c2b8f9e40ba192ccddb1c81a9f5be2369e
Daniel Moreno <daniel.moreno@tesselo.com>
2021-08-17 20:13:17 +0200

    feat(webhooks): add security mechanism for asana webhooks

--- giges/handlers/asana.py
+++ giges/handlers/asana.py
@@ -1,7 +1,12 @@
+import hashlib
+import hmac
 from typing import Dict, Tuple, Union
 
 import structlog
 from connexion import request
+
+from giges.db import db
+from giges.models.asana import Event, Webhook
 
 logger = structlog.get_logger(__name__)
 
@@ -11,20 +16,49 @@ def projects() -> Union[Tuple[Dict, int], Tuple[Dict, int, Dict[str, str]]]:
     Handle incoming webhooks from asana related to projects.
 
     When registering a webhook, asana will first send a request with a secret,
-    X-Hook-Secret, that we can use later to verify the authenticity of the
-    rest of the webhooks requests using X-Hook-Signature.
-
-    But we are not doing such check for the first implementation.
+    X-Hook-Secret, that we use to verify the authenticity of the rest of the
+    webhooks requests using X-Hook-Signature.
 
     :return: - a tuple with an empty body and the status
              - a tuple with an empty body, the status and the handshake header
     """
-    if not request.headers.get("X-Hook-Secret") and not request.headers.get(
-        "X-Hook-Signature"
-    ):
-        return {}, 400
-    if request.headers.get("X-Hook-Secret"):
-        return {}, 204, {"X-Hook-Secret": request.headers["X-Hook-Secret"]}
+    secret = request.headers.get("X-Hook-Secret")
+    signature = request.headers.get("X-Hook-Signature")
 
-    logger.info(request.json["events"])
+    if not secret and not signature:
+        return {"msg": "Invalid parameters"}, 400
+    webhook = Webhook.query.filter_by(path=request.path).one_or_none()
+    if not webhook:
+        return {"msg": "The webhook was not configured"}, 404
+    if secret:
+        if webhook.secret:
+            logger.warning(
+                "HACKING ATTEMPT: re-write of webhook secret",
+                headers=request.headers,
+            )
+            return {"msg": "Hacking not allowed"}, 400
+        webhook.secret = secret
+        db.session.add(webhook)
+        db.session.commit()
+
+        return {}, 204, {"X-Hook-Secret": secret}
+
+    if not webhook.secret:
+        return {"msg": "The webhook does not have a secret"}, 500
+
+    signature = hmac.new(
+        webhook.secret.encode("ascii", "ignore"),
+        msg=request.data,
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+    if not hmac.compare_digest(signature, request.headers["X-Hook-Signature"]):
+        logger.warning(
+            "HACKING ATTEMPT: webhook event with non matching signature",
+            headers=request.headers,
+        )
+        return {"msg": "Hacking not allowed"}, 400
+
+    event = Event(webhook=webhook, content=request.json["events"])
+    db.session.add(event)
+    db.session.commit()
     return {}, 204
--- test/integration/test_asana.py
+++ test/integration/test_asana.py
@@ -1,20 +1,92 @@
-def test_asana_projects_handshake(client):
+from giges.models.asana import Event, Webhook
+
+
+def test_asana_projects_handshake(client, transactional_db):
+    secret = "wow_such_very_secret"
+    webhook = Webhook(path="/asana/projects")
+    transactional_db.add(webhook)
+    transactional_db.commit()
+
     response = client.post(
         "/asana/projects",
-        headers={"X-Hook-Secret": "wow_such_very_secret"},
+        headers={"X-Hook-Secret": secret},
         json={},
     )
     assert response.status_code == 204
-    assert response.headers["X-Hook-Secret"] == "wow_such_very_secret"
+    assert response.headers["X-Hook-Secret"] == secret
+    assert webhook.secret == secret
 
 
-def test_asana_project_event_ok(client):
+def test_asana_projects_handshake_no_webhook(client):
+    response = client.post(
+        "/asana/projects",
+        headers={"X-Hook-Secret": "whatever"},
+        json={},
+    )
+    assert response.status_code == 404
+
+
+def test_asana_projects_handshake_hack_attempt(client, transactional_db):
+    secret = "wow_such_very_secret"
+    webhook = Webhook(path="/asana/projects", secret=secret)
+    transactional_db.add(webhook)
+    transactional_db.commit()
+
+    response = client.post(
+        "/asana/projects",
+        headers={"X-Hook-Secret": "evil hacker"},
+        json={},
+    )
+    assert response.status_code == 400
+    assert webhook.secret == secret
+
+
+def test_asana_project_event_no_secret(client, transactional_db):
+    webhook = Webhook(path="/asana/projects")
+    transactional_db.add(webhook)
+    transactional_db.commit()
+
     response = client.post(
         "/asana/projects",
         headers={"X-Hook-Signature": "wow_such_very_secret_signature"},
         json={"events": [{"glitch": "in matrix"}]},
     )
+    assert response.status_code == 500
+    assert Event.query.count() == 0
+
+
+def test_asana_project_event_bad_signature(client, transactional_db):
+    webhook = Webhook(path="/asana/projects", secret="pork_fillet")
+    transactional_db.add(webhook)
+    transactional_db.commit()
+
+    response = client.post(
+        "/asana/projects",
+        headers={
+            "X-Hook-Signature": "I am a bad cracker wanting to crack the pipe"
+        },
+        json={"events": [{"glitch": "in matrix"}]},
+    )
+    assert response.status_code == 400
+    assert Event.query.count() == 0
+
+
+def test_asana_project_event_ok(client, transactional_db):
+    signature = (
+        "56302af223b5cfad770ff07469189e5a2ac961bf77ac6fcf000ae75c619af3d7"
+    )
+    assert Event.query.count() == 0
+    webhook = Webhook(path="/asana/projects", secret="pork_fillet")
+    transactional_db.add(webhook)
+    transactional_db.commit()
+
+    response = client.post(
+        "/asana/projects",
+        headers={"X-Hook-Signature": signature},
+        json={"events": [{"glitch": "in matrix"}]},
+    )
     assert response.status_code == 204
+    assert Event.query.count() == 1
 
 
 def test_asana_project_event_no_events(client):
