ea1e3d925fe9d33bba548bbcf3f8c6a26c080f0b
Daniel Moreno <daniel.moreno@tesselo.com>
2021-08-27 13:19:36 +0200

    refactor(tests): use pytest factoryboy to create objects

--- test/integration/test_asana.py
+++ test/integration/test_asana.py
@@ -1,11 +1,10 @@
-from giges.models.asana import Event, Webhook
+from giges.models.asana import Event
 
 
-def test_asana_projects_handshake(client, transactional_db):
+def test_asana_projects_handshake(client, webhook):
     secret = "wow_such_very_secret"
-    webhook = Webhook(path="/asana/projects")
-    transactional_db.add(webhook)
-    transactional_db.commit()
+    webhook.path = "/asana/projects"
+    webhook.secret = None
 
     response = client.post(
         "/asana/projects",
@@ -26,14 +25,11 @@ def test_asana_projects_handshake_no_webhook(client):
     assert response.status_code == 404
 
 
-def test_asana_projects_handshake_hack_attempt(client, transactional_db):
-    secret = "wow_such_very_secret"
-    webhook = Webhook(path="/asana/projects", secret=secret)
-    transactional_db.add(webhook)
-    transactional_db.commit()
+def test_asana_projects_handshake_hack_attempt(client, webhook):
+    secret = webhook.secret
 
     response = client.post(
-        "/asana/projects",
+        webhook.path,
         headers={"X-Hook-Secret": "evil hacker"},
         json={},
     )
@@ -41,13 +37,10 @@ def test_asana_projects_handshake_hack_attempt(client, transactional_db):
     assert webhook.secret == secret
 
 
-def test_asana_project_event_no_secret(client, transactional_db):
-    webhook = Webhook(path="/asana/projects")
-    transactional_db.add(webhook)
-    transactional_db.commit()
-
+def test_asana_project_event_no_secret(client, webhook):
+    webhook.secret = None
     response = client.post(
-        "/asana/projects",
+        webhook.path,
         headers={"X-Hook-Signature": "wow_such_very_secret_signature"},
         json={"events": [{"glitch": "in matrix"}]},
     )
@@ -55,13 +48,9 @@ def test_asana_project_event_no_secret(client, transactional_db):
     assert Event.query.count() == 0
 
 
-def test_asana_project_event_bad_signature(client, transactional_db):
-    webhook = Webhook(path="/asana/projects", secret="pork_fillet")
-    transactional_db.add(webhook)
-    transactional_db.commit()
-
+def test_asana_project_event_bad_signature(client, webhook):
     response = client.post(
-        "/asana/projects",
+        webhook.path,
         headers={
             "X-Hook-Signature": "I am a bad cracker wanting to crack the pipe"
         },
@@ -71,17 +60,15 @@ def test_asana_project_event_bad_signature(client, transactional_db):
     assert Event.query.count() == 0
 
 
-def test_asana_project_event_ok(client, transactional_db):
+def test_asana_project_event_ok(client, webhook):
     signature = (
         "56302af223b5cfad770ff07469189e5a2ac961bf77ac6fcf000ae75c619af3d7"
     )
     assert Event.query.count() == 0
-    webhook = Webhook(path="/asana/projects", secret="pork_fillet")
-    transactional_db.add(webhook)
-    transactional_db.commit()
+    webhook.secret = "pork_fillet"
 
     response = client.post(
-        "/asana/projects",
+        webhook.path,
         headers={"X-Hook-Signature": signature},
         json={"events": [{"glitch": "in matrix"}]},
     )
@@ -97,6 +84,6 @@ def test_asana_project_event_no_events(client):
     assert response.status_code == 400
 
 
-def test_asana_project_event_no_signature(client):
-    response = client.post("/asana/projects", json={"events": []})
+def test_asana_project_event_no_signature(client, webhook):
+    response = client.post(webhook.path, json={"events": []})
     assert response.status_code == 400
