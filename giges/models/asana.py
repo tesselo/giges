0c77f4ceb436c3338e90b81c4c9e17c23c108f70
Daniel Moreno <daniel.moreno@tesselo.com>
2021-10-04 17:17:57 +0200

    feat(db): asana task and task change models

--- giges/models/asana.py
+++ giges/models/asana.py
@@ -1,10 +1,21 @@
 import enum
+from typing import Dict, List
 
-from sqlalchemy import CHAR, JSON, TIMESTAMP, Column, Enum, ForeignKey, String
+from sqlalchemy import (
+    CHAR,
+    JSON,
+    TIMESTAMP,
+    Boolean,
+    DateTime,
+    Enum,
+    ForeignKey,
+    String,
+)
 from sqlalchemy.orm import relationship
 
 from ..db import db
-from .mixins import UUIDMixin
+from ..settings import asana_fields
+from .mixins import TimestampMixin, UUIDMixin
 
 
 class ResourceTypeEnum(str, enum.Enum):
@@ -28,12 +39,12 @@ class Project(db.Model, UUIDMixin):
     name = db.Column(
         String, nullable=False, index=True, doc="Name of the project"
     )
-    created_at = Column(
+    created_at = db.Column(
         TIMESTAMP,
         nullable=False,
         doc="When the project was created in Asana",
     )
-    updated_at = Column(
+    updated_at = db.Column(
         TIMESTAMP,
         nullable=False,
         doc="When the project was modified in Asana",
@@ -75,7 +86,87 @@ class Webhook(db.Model, UUIDMixin):
     project = relationship("Project")
 
 
-class Event(db.Model, UUIDMixin):
+class Task(db.Model, UUIDMixin, TimestampMixin):
+    __tablename__ = "asana_task"
+
+    external_id = db.Column(
+        String,
+        index=True,
+        doc="The ID of the Task in Asana",
+    )
+
+    name = db.Column(
+        String, nullable=False, index=True, doc="Name of the task"
+    )
+    description = db.Column(String, index=True, doc="Description of the task")
+    completed = db.Column(Boolean)
+    completed_at = db.Column(DateTime)
+
+    class_of_service = db.Column(String)
+    task_progress = db.Column(String)
+    item_category = db.Column(String)
+    related_service = db.Column(String)
+    section = db.Column(String)
+
+    def save_custom_fields(self, custom_fields: List[Dict]) -> None:
+        """
+        Mapping between the Asana custom fields and the information we want
+        to save and track into the database.
+
+        :param custom_fields: dict from Asana show task response
+        :return: Nope
+        """
+        for field in custom_fields:
+            field_id = field.get("gid")
+            if field_id in asana_fields.keys():
+                if field.get("enum_value"):
+                    value = asana_fields[field_id]["options"][
+                        field["enum_value"]["gid"]
+                    ]
+                    setattr(self, asana_fields[field_id]["column"], value)
+
+
+class TaskChange(db.Model, UUIDMixin, TimestampMixin):
+    __tablename__ = "asana_task_change"
+
+    task_id = db.Column(
+        CHAR(36),
+        ForeignKey(
+            "asana_task.id",
+            deferrable=True,
+            initially="DEFERRED",
+            name="asana_task_change_task_fk",
+        ),
+    )
+
+    class_of_service = db.Column(String)
+    task_progress = db.Column(String)
+    item_category = db.Column(String)
+    related_service = db.Column(String)
+    section = db.Column(String)
+
+    task = relationship("Task")
+
+    def save_task_changes(self) -> None:
+        """
+        Inspect the sqlalchemy object of a task to determine
+        and save the columns that changed.
+
+        :return: Nein
+        """
+        from sqlalchemy import inspect
+        from sqlalchemy.orm import class_mapper
+
+        inspected = inspect(self.task)
+        attrs = class_mapper(self.task.__class__).column_attrs
+        for attr in attrs:
+            if hasattr(self, attr.key) and attr.key != "id":
+                hist = getattr(inspected.attrs, attr.key).history
+                if hist.has_changes():
+                    setattr(self, attr.key, getattr(self.task, attr.key))
+
+
+class Event(db.Model, UUIDMixin, TimestampMixin):
     __tablename__ = "asana_event"
 
     webhook_id = db.Column(
