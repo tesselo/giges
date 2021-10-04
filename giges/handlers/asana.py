e5b3c6ab858a6b914b1b52607de67dce518d6e18
Daniel Moreno <daniel.moreno@tesselo.com>
2021-10-04 17:19:02 +0200

    feat(asana): save and track asana task changes

--- giges/handlers/asana.py
+++ giges/handlers/asana.py
@@ -1,14 +1,78 @@
 import hashlib
 import hmac
-from typing import Dict, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 
+import asana
+import dateutil
 import structlog
 from connexion import request
+from flask import current_app
 
 from giges.db import db
-from giges.models.asana import Event, Project, Webhook
+from giges.models.asana import Event, Project, Task, TaskChange, Webhook
 
 logger = structlog.get_logger(__name__)
+
+
+def retrieve_task_information(asana_id: str) -> Dict[str, Any]:
+    """
+    Contacts Asana and download all the information from a task.
+
+    :param asana_id: the external (Asana) ID of the task
+    :return: the dictionary from the JSON parsed information
+    """
+    client = asana.Client.access_token(current_app.config["ASANA_TOKEN"])
+    return client.tasks.find_by_id(asana_id)
+
+
+def update_task(task: Task, asana_task: Dict[str, Any]) -> TaskChange:
+    """
+    Set the values of a task to the current ones in  Asana.
+
+    :param task: the task to update
+    :param asana_task: the current task information in Asana
+    :return: the object containing the task changes
+    """
+    task.completed = asana_task.get("completed")
+    if asana_task.get("completed_at"):
+        task.completed_at = dateutil.parser.parse(
+            asana_task.get("completed_at")
+        )
+    task.name = asana_task.get("name")
+    task.description = asana_task.get("description")
+    if len(asana_task["memberships"]):
+        task.section = asana_task["memberships"][0]["section"]["name"]
+    task.save_custom_fields(asana_task["custom_fields"])
+
+    task_change = TaskChange(task=task)
+    task_change.save_task_changes()
+
+    return task_change
+
+
+def handle_task_events(
+    webhook: Webhook, events: List[Dict[str, Dict]]
+) -> None:
+    """
+    Resolves events from webhooks into tasks and tasks changes.
+
+    :param webhook: the handled webhook
+    :param events: the list of events given by Asana
+    """
+    task_gids = set()
+    for event in events:
+        task_gids.add(event["resource"]["gid"])
+
+    for task_gid in task_gids:
+        task = Task.query.filter_by(external_id=task_gid).one_or_none()
+        if task is None:
+            task = Task(external_id=task_gid)
+        asana_task = retrieve_task_information(task_gid)
+        task_change = update_task(task, asana_task)
+        db.session.add(task)
+        db.session.add(task_change)
+    db.session.add(Event(webhook=webhook, content=events))
+    db.session.commit()
 
 
 def task_webhook(
@@ -16,6 +80,10 @@ def task_webhook(
 ) -> Union[Tuple[Dict, int], Tuple[Dict, int, Dict[str, str]]]:
     """
     Handle incoming webhooks of modified tasks from an asana project.
+
+    When registering a webhook, asana will first send a request with a secret,
+    X-Hook-Secret, that we use to verify the authenticity of the rest of the
+    webhooks requests using X-Hook-Signature.
 
     :param project_id: the external (asana) ID of the project
     :return: - a tuple with an empty body and the status
@@ -29,38 +97,7 @@ def task_webhook(
         return {"msg": "The project was not configured"}, 404
     if webhook.project != project:
         return {"msg": "The webhook does not match the targeted project"}, 400
-    return handle_webhook(webhook)
 
-
-def project_webhook() -> Union[
-    Tuple[Dict, int], Tuple[Dict, int, Dict[str, str]]
-]:
-    """
-    Handle incoming webooks of new projects from asana.
-
-    :return: - a tuple with an empty body and the status
-             - a tuple with an empty body, the status and the handshake header
-    """
-    webhook = Webhook.query.filter_by(path=request.path).one_or_none()
-    if not webhook:
-        return {"msg": "The webhook was not configured"}, 404
-    return handle_webhook(webhook)
-
-
-def handle_webhook(
-    webhook: Webhook,
-) -> Union[Tuple[Dict, int], Tuple[Dict, int, Dict[str, str]]]:
-    """
-    Handle incoming webhooks from asana.
-
-    When registering a webhook, asana will first send a request with a secret,
-    X-Hook-Secret, that we use to verify the authenticity of the rest of the
-    webhooks requests using X-Hook-Signature.
-
-    :param webhook: the instance of the webhook in Giges
-    :return: - a tuple with an empty body and the status
-             - a tuple with an empty body, the status and the handshake header
-    """
     secret = request.headers.get("X-Hook-Secret")
     signature = request.headers.get("X-Hook-Signature")
 
@@ -94,7 +131,13 @@ def handle_webhook(
         )
         return {"msg": "Hacking not allowed"}, 400
 
-    event = Event(webhook=webhook, content=request.json["events"])
-    db.session.add(event)
-    db.session.commit()
+    if len(request.json["events"]) == 0:
+        # Asana periodically ping the endpoint without events
+        return {}, 204
+
+    try:
+        handle_task_events(webhook, request.json["events"])
+    except KeyError:
+        return {"msg": "Incorrect event format"}, 400
+
     return {}, 204
